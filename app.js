594abf98d002a9b1376f8b588824493ea65567af
Antkeo1 <sleav143@gmail.com>
2019-09-28 09:25:42 -0400

    user route nd controller created

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//user routes
+const userRoutes = require('./routes/user');
+
+// our app
 const app = express();
 
-app.get('/', (req, res) => {
-    
+//db
+mongoose.connect(process.env.DATABASE, {
+   useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connected'))
+
+//routes middleware
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(` Server is running on port ${port}`)
 })
